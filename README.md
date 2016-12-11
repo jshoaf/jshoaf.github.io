0bee5aad9600bb17dafd9d7859223a8144782e27
Jonathan Shoaf <jonathan.shoaf@gmail.com>
2016-12-11 00:37:13 -0500

    Added Bootstrap and SASS

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # jshoaf.github.io
 
 package.json: https://docs.npmjs.com/files/package.json
+
+#Required
+- npm install -g bower
+- npm init
+- bower init
+- npm install --global gulp-cli
+- npm install --save-dev gulp
+- npm install --save-dev gulp-sass
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var webserver = require('gulp-webserver');
 
 // https://www.npmjs.com/package/gulp-webserver
@@ -12,6 +13,20 @@ gulp.task('webserver', function() {
     }));
 });
 
-gulp.task('default', function() {
+gulp.task('default', ['sass'], function() {
+  gulp.watch('scss/**/*');
   gulp.run('webserver');
+});
+
+gulp.task('sass', function() {
+
+  return gulp.src('scss/main.scss')
+    .pipe(sass({
+        outputStyle: 'nested',
+        precison: 3,
+        errLogToConsole: true,
+        includePaths: ['./bower_components/bootstrap-sass/assets/stylesheets']
+    }))
+    .pipe(gulp.dest('css/'));
+
 });

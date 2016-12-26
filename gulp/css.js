471b25351dac3ad48f25ee16d813a640784302db
Jonathan Shoaf <jonathan.shoaf@gmail.com>
2016-12-25 19:54:08 -0500

    Minify CSS

--- gulp/css.js
+++ gulp/css.js
@@ -6,7 +6,7 @@ module.exports = function () {
 
     gulp.src('scss/*')
         .pipe(sass({
-            outputStyle: 'nested',
+            outputStyle: 'compressed',
             precison: 3,
             errLogToConsole: true,
             includePaths: ['./bower_components/font-awesome/scss', './bower_components/bootstrap-sass/assets/stylesheets']
